99ffbed248fe53f9a047bd4511eee263b38fde55
odd02311 <jaju1995@naver.com>
2023-09-18 05:08:41 +0900

    qerydls 사용할수 있는 환경구성을 설정 후 querydslPredicateExecutor, querydlsBinderCustomizer를 이용하여 겅색기능 추가

--- src/main/java/com/teampj/boardExample/repository/ArticleCommentRepository.java
+++ src/main/java/com/teampj/boardExample/repository/ArticleCommentRepository.java
@@ -1,9 +1,30 @@
 package com.teampj.boardExample.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.teampj.boardExample.domain.Article;
 import com.teampj.boardExample.domain.ArticleComment;
+import com.teampj.boardExample.domain.QArticle;
+import com.teampj.boardExample.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+    JpaRepository<ArticleComment, Long>,
+    QuerydslPredicateExecutor<ArticleComment>, // Article 엔티티 안에있는 모든 필드에 대한 검색 기능 추가
+    QuerydslBinderCustomizer<QArticleComment> //
+{
+  @Override
+  default void customize(QuerydslBindings bindings, QArticleComment root){
+    bindings.excludeUnlistedProperties(true);
+    bindings.including(root.content, root.createdAt, root.createdBy); // 검색 필터
+    bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+    bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+  }
+
 }
--- src/main/java/com/teampj/boardExample/repository/ArticleRepository.java
+++ src/main/java/com/teampj/boardExample/repository/ArticleRepository.java
@@ -1,10 +1,32 @@
 package com.teampj.boardExample.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.teampj.boardExample.domain.Article;
+import com.teampj.boardExample.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+    JpaRepository<Article, Long>,
+    QuerydslPredicateExecutor<Article>,
+    QuerydslBinderCustomizer<QArticle>
+{
+  @Override
+  default void customize(QuerydslBindings bindings, QArticle root){
+      bindings.excludeUnlistedProperties(true);
+      bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); // 검색 필터
+//      bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // 쿼리 생성 문자 == like '%{v}'
+      bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+      bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // 대소문자 구문 x | 쿼리 생성 문장 like '%${v}%'
+      bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+      bindings.bind(root.createdAt).first(DateTimeExpression::eq); // 문자열 x datetimeexpression | 적절한 검사가 없어서 eq로 놔둠
+      bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+  }
+
 }
